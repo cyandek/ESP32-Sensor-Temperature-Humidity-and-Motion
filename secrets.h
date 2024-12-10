a53868109ddc625427ea128cbc5553b2f9b17191
cyandek <158543011+cyandek@users.noreply.github.com>
2024-12-10 06:29:55 -0600

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+#define MQTT_USER "USERNAME"
+#define MQTT_PASSWORD "PASSWORD"
+const char *mqtt_server = "IPADDRESS";
+
+const char *Temp_Topic = "esp32/office/temperature";
+const char *Humid_Topic = "esp32/office/humidity";
+const char *Motion_Topic = "esp32/office/motion";
